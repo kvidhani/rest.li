55eb982da5cd6c1fd396e44ed1881049a32c2a61
Karim Vidhani <kvidhani@linkedin.com>
2015-06-04 19:03:37 -0700

    finish up initial writer

--- multipart-mime/src/main/java/com/linkedin/multipart/writer/MultiPartMIMEUtils.java
+++ multipart-mime/src/main/java/com/linkedin/multipart/writer/MultiPartMIMEUtils.java
@@ -11,6 +11,9 @@ import java.util.Random;
  */
 class MultiPartMIMEUtils {
 
+  static final String CONTENT_TYPE_HEADER = "Content-Type";
+  static final String MULTIPART_PREFIX = "multipart/";
+  static final String BOUNDARY_PARAMETER = "boundary";
   static final byte[] CRLF = "\r\n".getBytes();
   private static final char[] MULTIPART_CHARS =
       "-_1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
@@ -22,7 +25,9 @@ class MultiPartMIMEUtils {
       headerBuffer.append(formattedHeader(header.getKey(), header.getValue()));
     }
 
-    //Headers should always be 7 bit ASCII according to the RFC
+    //Headers should always be 7 bit ASCII according to the RFC. If characters provided in the header
+    //are do not constitute a valid ASCII character, then this Charset will place (U+FFFD) or the
+    //replacement character which is used to replace an unknown or unrepresentable character.
     return ByteString.copyString(headerBuffer.toString(), Charset.forName("US-ASCII"));
   }
 
@@ -30,7 +35,8 @@ class MultiPartMIMEUtils {
     return ((name == null ? "" : name) + ": " + (null == value ? "" : value) + CRLF);
   }
 
-  static String generateBoundary() {
+  static String generateBoundary()
+  {
     final StringBuilder buffer = new StringBuilder();
     final Random rand = new Random();
     //The RFC limit is 70 characters, so we will create a boundary that is randomly
@@ -39,8 +45,27 @@ class MultiPartMIMEUtils {
     for (int i = 0; i < count; i++) {
       buffer.append(MULTIPART_CHARS[rand.nextInt(MULTIPART_CHARS.length)]);
     }
-    //RFC 2046 states a limited character set for the boundary
-    //Which means we can encode using ASCII todo
+    //RFC 2046 states a limited character set for the boundary but we don't have to explicitly encode to ASCII
+    //since Unicode is backward compatible with ASCII
     return buffer.toString();
+  }
+
+  static String buildContentTypeHeader(final String mimeType, final String boundary,
+                                       final Map<String, String> contentTypeParameters) {
+
+    final StringBuilder contentTypeBuilder = new StringBuilder();
+    contentTypeBuilder.append(MULTIPART_PREFIX).append(mimeType);
+    //As per the RFC, parameters of the Content-Type header are separated by semi colons
+    contentTypeBuilder.append("; ").append(BOUNDARY_PARAMETER).append("=").append(boundary);
+
+    for (final Map.Entry<String, String> parameter : contentTypeParameters.entrySet()) {
+      //Note we ignore the provided boundary parameter
+      if(!parameter.getKey().trim().equalsIgnoreCase(BOUNDARY_PARAMETER)) {
+        contentTypeBuilder.append("; ").append(parameter.getKey().trim()).append("=")
+            .append(parameter.getValue().trim());
+      }
+    }
+
+    return contentTypeBuilder.toString();
   }
 }
