2fb316622b9c1fdf29a91d0469da39a0056ab8de
Karim Vidhani <kvidhani@kvidhani-mn1.linkedin.biz>
2015-06-05 13:17:09 -0700

    initial reader impl

---
+++ multipart-mime/src/main/java/com/linkedin/multipart/reader/MultiPartMIMEReaderCallback.java
@@ -0,0 +1,26 @@
+package com.linkedin.multipart.reader;
+
+/**
+ * Created by kvidhani on 6/5/15.
+ */
+public interface MultiPartMIMEReaderCallback {
+    //Upon construction of a MultiPartReader or upon a call to registerReaderCallback()
+    //inside of the MultiPartMIMEReader this callback will be invoked
+    //(if there are parts in this stream).
+    //If a part had just finished, meaning onCurrentPartSuccessfullyFinished() was invoked
+    //in the SinglePartMIMEReader callback, then this will be invoked if there are
+    //future parts available.
+    //Note that when this is called, it does not bind the callee to acquire and
+    //commit to reading this part. Only upon registering with the SinglePartMIMEReader (init())
+    //does the client then commit to reading that part.
+    void onNewPart(MultiPartMIMEReader.SinglePartMIMEReader singleParMIMEReader);
+
+    //When the entire stream is finished
+    void onFinished();
+
+    //When all parts are abandoned
+    void onAbandoned();
+
+    //When there is an error reading from the stream.
+    void onStreamError(Throwable e);
+}
---
+++ multipart-mime/src/main/java/com/linkedin/multipart/reader/exceptions/PartBindException.java
@@ -0,0 +1,7 @@
+package com.linkedin.multipart.reader.exceptions;
+
+/**
+ * Created by kvidhani on 6/5/15.
+ */
+public class PartBindException extends RuntimeException {
+}
