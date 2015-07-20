85d1096f9c782c2ff5ab07b19fbf34cccd29ddd9
kvidhani <kvidhani@linkedin.com>
2015-07-19 19:58:47 -0700

    wrapping up multipart mime inputstream testing

--- multipart-mime/src/main/java/com/linkedin/multipart/MultiPartMIMEInputStream.java
+++ multipart-mime/src/main/java/com/linkedin/multipart/MultiPartMIMEInputStream.java
@@ -23,6 +23,8 @@ import java.util.concurrent.TimeoutException;
 
   //todo use a delegate pattern to forbid external users from calling interface methods directly
   //todo what needs to be volatile?
+
+  //todo what happens if some data source keeps on reading only 0 bytes
 public final class MultiPartMIMEInputStream implements MultiPartMIMEDataSource
 {
   public static final int DEFAULT_MAXIMUM_BLOCKING_DURATION = 3000;
@@ -31,7 +33,7 @@ public final class MultiPartMIMEInputStream implements MultiPartMIMEDataSource
   private volatile WriteHandle _writeHandle;
   private final Map<String, String> _headers;
   private final InputStream _inputStream;
-  private boolean _dataSourceFinished = false; //since there is no way to see if an InputStream has already been closed
+  private volatile boolean _dataSourceFinished = false; //since there is no way to see if an InputStream has already been closed
   private final ExecutorService _executorService;
   private final int _maximumBlockingTime;
   private final int _writeChunkSize;
@@ -44,30 +46,10 @@ public final class MultiPartMIMEInputStream implements MultiPartMIMEDataSource
 
   @Override
   public void onWritePossible() {
-
-    //We can't use the traditional while (_writeHandle.remaining() > 0) since we need to incrementally
-    //schedule reads. We need to wait for each one to finish before moving onto the next. Moving onto the next
-    //means scheduling a runnable which will call onWritePossible() once again simulating what R2 would do.
-    if (_writeHandle.remaining() > 0) {
-      final CountDownLatch latch = new CountDownLatch(1);
-
-      //We use two threads from the client provided thread pool. We must use this technique
-      //because if the read from the input stream hangs, there is no way to recover.
-      //Therefore we have a reader thread and a boss thread that waits for the reader to complete.
-
-      //We want to invoke the callback on only one (boss) thread because:
-      //1. It makes the threading model more complicated if the reader thread invokes the callbacks as well as
-      //the boss thread.
-      //2. In cases where there is a timeout, meaning the read took too long, we want to close the
-      //input stream on the boss thread to unblock the reader thread pool thread. In such cases it is indeterminate
-      //as to what the behavior is when the aforementioned blocked thread wakes up. This thread could receive an IOException
-      //or come back with a few bytes. Due to this indeterminate behavior we don't want to trust the reader thread
-      //to invoke any callbacks. We want our boss thread to explicitly invoke error() in such cases.
-      final InputStreamReader inputStreamReader = new InputStreamReader(latch);
-      final InputStreamReaderManager inputStreamReaderManager = new InputStreamReaderManager(inputStreamReader, latch);
-      _executorService.submit(inputStreamReader);
-      _executorService.submit(inputStreamReaderManager);
-    }
+    //Delegate the while (writeHandle.remaining() > 0) task to the input stream reader manager task.
+    //Its considerably easier this way then spawning a new manager task for each write available on
+    //the writeHandle.
+    _executorService.submit(new InputStreamReaderManager());
   }
 
   /**
@@ -86,6 +68,111 @@ public final class MultiPartMIMEInputStream implements MultiPartMIMEDataSource
   @Override
   public Map<String, String> dataSourceHeaders() {
     return _headers;
+  }
+
+  private class InputStreamReaderManager implements Runnable {
+
+    private InputStreamReaderManager() {
+    }
+
+    @Override
+    public void run() {
+
+      //We use two threads from the client provided thread pool. We must use this technique
+      //because if the read from the input stream hangs, there is no way to recover.
+      //Therefore we have a reader thread and a boss thread that waits for the reader to complete.
+
+      //We want to invoke the callback on only one (boss) thread because:
+      //1. It makes the threading model more complicated if the reader thread invokes the callbacks as well as
+      //the boss thread.
+      //2. In cases where there is a timeout, meaning the read took too long, we want to close the
+      //input stream on the boss thread to unblock the reader thread pool thread. In such cases it is indeterminate
+      //as to what the behavior is when the aforementioned blocked thread wakes up. This thread could receive an IOException
+      //or come back with a few bytes. Due to this indeterminate behavior we don't want to trust the reader thread
+      //to invoke any callbacks. We want our boss thread to explicitly invoke error() in such cases.
+
+      while (_writeHandle.remaining() > 0) {
+        //Note that we follow the orthodox writer pattern here by honoring all writes available on the writeHandle.
+        //Furthermore this logic will guarantee serial and sequential reading through the input stream.
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        final InputStreamReader inputStreamReader = new InputStreamReader(latch);
+        _executorService.submit(inputStreamReader);
+
+        try {
+          boolean nonTimeoutFinish = latch.await(_maximumBlockingTime, TimeUnit.MILLISECONDS);
+          if (nonTimeoutFinish) {
+            if (inputStreamReader._result != null) {
+              //If the call went through, extract the info and write
+              if (_dataSourceFinished) {
+                //No sense in writing an empty ByteString. This happens if we the data source was coincidentally
+                //evenly divisible by the chunk size OR the input stream returned 0 bytes.
+                if (!inputStreamReader._result.equals(ByteString.empty())) {
+                  _writeHandle.write(inputStreamReader._result);
+                }
+                _writeHandle.done();
+                //Close the stream since we won't be invoked again
+                try {
+                  _inputStream.close();
+                } catch (IOException ioException) {
+                  //Safe to swallow
+                  //An exception thrown when we try to close the InputStream should not really
+                  //make its way down as an error...
+                }
+                //Break here, even though there may be more writes on the writeHandle.
+                //We cannot continue writing if our data source has finished.
+                break;
+              } else {
+                //Just a normal write
+                _writeHandle.write(inputStreamReader._result);
+              }
+            } else {
+              //This means the result is null which implies
+              //that a throwable exists. In this case the read on the InputStream threw an IOException
+              //First close the InputStream
+              try {
+                _inputStream.close();
+              } catch (IOException ioException) {
+                //Safe to swallow
+                //An exception thrown when we try to close the InputStream should not really
+                //make its way down as an error...
+              }
+              //Now mark is it an error using the correct throwable
+              _writeHandle.error(inputStreamReader._error);
+              //Break here, even though there may be more writes on the writeHandle.
+              //We cannot continue writing in this condition.
+              break;
+            }
+          } else {
+            //there was a timeout when trying to read
+            try {
+              //Close the input stream here since we won't be doing any more reading. This should also potentially
+              //free up any threads blocked on the read()
+              _inputStream.close();
+            } catch (IOException ioException) {
+              //Safe to swallow
+            }
+            _writeHandle.error(new TimeoutException("InputStream reading timed out"));
+            //Break here, even though there may be more writes on the writeHandle.
+            //We cannot continue writing in this condition.
+            break;
+          }
+        } catch (InterruptedException exception) {
+
+          //If this thread interrupted, then we have no choice but to abort everything.
+          try {
+            //Close the input stream here since we won't be doing any more reading.
+            _inputStream.close();
+          } catch (IOException ioException) {
+            //Safe to swallow
+          }
+          _writeHandle.error(exception);
+          //Break here, even though there may be more writes on the writeHandle.
+          //We cannot continue writing in this condition.
+          break;
+        }
+      }
+    }
   }
 
   private class InputStreamReader implements Runnable {
@@ -127,101 +214,6 @@ public final class MultiPartMIMEInputStream implements MultiPartMIMEDataSource
 
   }
 
-  private class InputStreamReaderManager implements Runnable {
-
-    private final InputStreamReader _inputStreamReader;
-    private final CountDownLatch _countDownLatch;
-
-    private InputStreamReaderManager(final InputStreamReader inputStreamReader, final CountDownLatch countDownLatch) {
-      _inputStreamReader = inputStreamReader;
-      _countDownLatch = countDownLatch;
-    }
-
-    @Override
-    public void run() {
-
-      try {
-        boolean successful = _countDownLatch.await(_maximumBlockingTime, TimeUnit.MILLISECONDS);
-        if (successful) {
-          if (_inputStreamReader._result != null) {
-            //If the call went through, extract the info and write
-            if (_dataSourceFinished) {
-              _writeHandle.write(_inputStreamReader._result);
-              _writeHandle.done();
-              //Close the stream since we won't be invoked again
-              try {
-                _inputStream.close();
-              } catch (IOException ioException) {
-                //Safe to swallow
-                //An exception thrown when we try to close the InputStream should not really
-                //make its way down as an error...
-              }
-            } else {
-              //Just a normal write
-              _writeHandle.write(_inputStreamReader._result);
-            }
-          } else {
-            //This means the result is null which implies
-            //that a throwable exists. In this case the read on the InputStream threw an IOException
-            //First close the InputStream
-            try {
-              _inputStream.close();
-            } catch (IOException ioException) {
-              //Safe to swallow
-              //An exception thrown when we try to close the InputStream should not really
-              //make its way down as an error...
-            }
-            //Now mark is it an error using the correct throwable
-            _writeHandle.error(_inputStreamReader._error);
-          }
-        } else {
-          //there was a timeout
-          try {
-            //Close the input stream here since we won't be doing any more reading. This should also potentially
-            //free up any threads blocked on the read()
-            _inputStream.close();
-          } catch (IOException ioException) {
-            //Safe to swallow
-          }
-          _writeHandle.error(new TimeoutException("InputStream reading timed out"));
-        }
-      } catch (InterruptedException exception) {
-
-        //If this thread interrupted, then we have no choice but to abort everything
-        try {
-          //Close the input stream here since we won't be doing any more reading.
-          _inputStream.close();
-        } catch (IOException ioException) {
-          //Safe to swallow
-        }
-        _writeHandle.error(exception);
-      } finally {
-
-        //Continue writing only if there is more to do. Note that orthodox writers typically have a
-        //while (_writeHandle.remaining() > 0) in onWritePossible(). Once _writeHandle.remaining() returns
-        //0, then R2 calls onWritePossible() again with a new remaining count.
-        //
-        //Since we can't follow that technique (since we have to sequentially read jobs from the input stream),
-        //we instead iterate sequentially using subsequent invocations to onWritePossible() which uses an
-        //if() loop instead.
-        //Therefore we continue only if:
-        //1. There is more to write on the writeHandle.
-        //AND
-        //2. We haven't called done yet.
-        if (_writeHandle.remaining() > 0 && _dataSourceFinished == false) {
-          final ContinueWritingManager continueWritingManager = new ContinueWritingManager();
-          _executorService.submit(continueWritingManager);
-        }
-      }
-    }
-  }
-
-  private class ContinueWritingManager implements Runnable {
-    @Override
-    public void run() {
-        MultiPartMIMEInputStream.this.onWritePossible();
-    }
-  }
 
     /**
    * Create a new instance of a MultiPartMIMEInputStream that wraps the provided InputStream to
--- multipart-mime/src/test/java/com/linkedin/multipart/AbstractMultiPartMIMEIntegrationStreamTest.java
+++ multipart-mime/src/test/java/com/linkedin/multipart/AbstractMultiPartMIMEIntegrationStreamTest.java
@@ -10,6 +10,16 @@ import test.r2.integ.AbstractStreamTest;
 /**
  * Created by kvidhani on 7/11/15.
  */
+
+//in rb mention:
+//        1. using writer directly in your interface - i.e composite writer
+//        2. the fact you are using the package private interface to hide public api details - could have also used abstract class….
+//        3. the writer has three constructors (part, single and multi)
+// 4. Can abort be called? MultiPartMIMEInputstream could be aborted in mid read...what to do? figure out what to do here
+    //and come bacck to this problme. ask zhenak or ang
+
+
+
 public abstract class AbstractMultiPartMIMEIntegrationStreamTest extends AbstractStreamTest {
 
     protected static Callback<StreamResponse> expectSuccessCallback(final CountDownLatch latch,
